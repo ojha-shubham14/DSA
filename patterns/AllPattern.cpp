2abe3e9dc95803b5329356786aab8d84346465a0
Shubham Ojha <shubham4clgg@gmail.com>
2026-03-11 20:53:49 +0530

    learnt inverted Pyramid

--- patterns/AllPattern.cpp
+++ patterns/AllPattern.cpp
@@ -129,9 +129,23 @@ void pattern13(int n){
         cout<<endl;
     }
 }
+void pattern14(int n){
+    for (int i=0; i<n; i++){
+        for(int j=0; j<i; j++){
+            cout << " ";
+        }
+        for(int j=0; j< 2*n -(2*i+1); j++){
+            cout << "*";
+        }
+        for(int j=0; j<i; j++){
+            cout << " ";
+        }
+        cout<<endl;
+    }
+}
 int main(){
     int n;
     cout << " Enter the number of lines for the Pattern :";
     cin>>n;
-    pattern12(n);
+    pattern14(n);
 }
