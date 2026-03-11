cc82389ea21853b3cdeeedeb767200e8c792bbce
Shubham Ojha <shubham4clgg@gmail.com>
2026-03-11 20:15:36 +0530

    cleaned the folder structure which was causing duplicate isseues

--- patterns/AllPattern.cpp
+++ patterns/AllPattern.cpp
@@ -115,9 +115,23 @@ void pattern12(int n){
         cout<<endl;
     }
 }
+void pattern13(int n){
+    for (int i=0; i<n; i++){
+        for(int j=0; j<n-i-1; j++){
+            cout << " ";
+        }
+        for(int j=0; j<2*i+1; j++){
+            cout << "*";
+        }
+        for(int j=0; j<n-i-1; j++){
+            cout << " ";
+        }
+        cout<<endl;
+    }
+}
 int main(){
     int n;
     cout << " Enter the number of lines for the Pattern :";
     cin>>n;
-    pattern11(n);
+    pattern12(n);
 }
