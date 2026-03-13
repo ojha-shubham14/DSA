e2bc748163d989cb6dd767f5dac7069faf9fbb3e
Shubham Ojha <shubham4clgg@gmail.com>
2026-03-14 00:29:54 +0530

    number space number pattern

--- patterns/AllPattern.cpp
+++ patterns/AllPattern.cpp
@@ -199,10 +199,28 @@ void pattern17(int n){
         cout<<endl;
     }
 }
+void pattern18(int n){
+    int space = 2* (n-1);
+   for (int i=1; i<=n; i++){
+    //numbers
+        for(int j=1; j<=i; j++){
+            cout << j;
+        }
+    //space
+        for(int j=1; j<=space; j++){
+            cout << " ";
+        }
+    //numbers
+        for(int j=i; j>=1; j--){
+            cout << j;
+        }
+        cout<<endl;
+        space -=2;
+    } 
+}
 int main(){
     int n;
     cout << " Enter the number of lines for the Pattern :";
     cin>>n;
-    pattern17(n);
-    cout<<"yaay learnt new pattern";
+    pattern18(n);
 }
