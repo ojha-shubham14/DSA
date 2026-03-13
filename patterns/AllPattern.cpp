9d1fa5f3feda7233aea9260e302b326f2137dd2c
Shubham Ojha <shubham4clgg@gmail.com>
2026-03-13 23:56:48 +0530

    learnt bainary right angled triangle

--- patterns/AllPattern.cpp
+++ patterns/AllPattern.cpp
@@ -183,9 +183,25 @@ void pattern16(int n){
         cout<<endl;
     }
 }
+void pattern17(int n){
+    int start = 1;
+    for(int i =0 ; i<n ; i++){
+        if(i%2==0){
+            start = 1;
+        }
+        else{
+            start =0;
+        }
+        for(int j=0;j<=i;j++){
+            cout<< start <<" ";
+            start = 1 - start ;
+        }
+        cout<<endl;
+    }
+}
 int main(){
     int n;
     cout << " Enter the number of lines for the Pattern :";
     cin>>n;
-    pattern16(n);
+    pattern17(n);
 }
