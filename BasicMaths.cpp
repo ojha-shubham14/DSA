8021b78ef99cea06ab9b765b24c3d52f313f83df
Shubham Ojha <shubham4clgg@gmail.com>
2026-03-17 22:44:48 +0530

    was easy but learnt some how new things

--- BasicMaths.cpp
+++ BasicMaths.cpp
@@ -9,10 +9,20 @@ int countNumbers(int n){
     cout<< "The total numbers of digits in these are : " << count;
     return count;
 }
+int reverseNumber(int n){
+    int reverse = 0 ;
+    while(n > 0){
+        int lastdigit =n % 10;
+        reverse = (reverse * 10)+ lastdigit; // first time (0*10)+lastdigit and in the next iteration it will be (previous closing value * 0) + lastdigit , i have done like this because in the 2nd turn reverse variable should become in 2 digits right ?
+        n = n / 10;
+    }
+    cout<< "The reverse of a given number is : "<<reverse;
+    return reverse;
+}
 int main(){
     int n;
-    cout<<"Enter the numbers to count the digits present in it : " ;
+    cout<<"Enter the numbers : " ;
     cin>>n;
-    countNumbers(n);
+    reverseNumber(n);
     return 0;
 }
