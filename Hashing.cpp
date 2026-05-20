8a0403f66c9e6aa3acd1f6894d3e13a8c9011fcf
Shubham Ojha <shubham4clgg@gmail.com>
2026-05-21 00:01:14 +0530

    did learnt the basic concept of hashing and did it via brute force technique

--- Hashing.cpp
+++ Hashing.cpp
@@ -1,2 +1,21 @@
 #include<iostream>
 using namespace std;
+//Brute force method to find the element in the array
+int f(int a[],int b){
+    int count =0;
+    int n;
+    cin>>n;
+    for(int i = 0;i<n ; i++){
+        if(a[i]==b){
+            count +=1;
+        }
+    }
+    return count;
+}
+
+int main(){
+    int a[]={1,2,3,44,4,6,4};
+    cout<<f(a,4);
+    return 0;
+    
+}
