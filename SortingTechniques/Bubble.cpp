f63b6e297f9b0d273bacf05f648cecf5138f2c8e
Shubham Ojha <shubham4clgg@gmail.com>
2026-09-01 03:48:54 +0530

    time commplexity check for bubble sort worst and best case

--- SortingTechniques/Bubble.cpp
+++ SortingTechniques/Bubble.cpp
@@ -2,12 +2,18 @@
 using namespace std;
 void bubble(int a[],int n){
     for(int i =0;i<n-1;i++){
+        int didswap = 0;
         for(int j=0;j<n-i-1;j++){
             if(a[j]>a[j+1]){
                 int temp = a[j];
                 a[j]=a[j+1];
                 a[j+1]=temp;
+                didswap =1;
             }
+            if(didswap == 0){
+                break;
+            }
+            cout<<"runs"<<"\n";
         }
     }
 }
